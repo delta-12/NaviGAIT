96e93da1e20b8f86078050120df199f4e6fa5e7a
delta-12 <btesq235@gmail.com>
2022-04-09 03:02:30 -0400

    update CORS policy

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# To Do
+
+- [ ] fix hardcoded urls
+- [ ] switch to more permanent domains
+- [ ] remove underline when hovering over links in navbar
--- upload_endpoint/app/server.js
+++ upload_endpoint/app/server.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const app = express()
 const videos = require("./routes/api/videos")
-var cors = require('cors');
+var cors = require("cors")
 
-app.use(cors())
+const corsOptions ={
+    origin: "https://navigait-web-app.herokuapp.com", 
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 
 app.use("/api/videos", videos)
 
