f3a9178c3e256410aa1968ddcf767f8c6e47d9d8
delta-12 <btesq235@gmail.com>
2022-04-10 23:56:18 -0400

    fix script to push dev code

--- push_code.sh
+++ push_code.sh
@@ -4,7 +4,5 @@ git pull
 git branch -M dev
 git add .
 read -p "Commit message: " msg
-echo $msg
-read -p "Commit and push code? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-git commit -m $msg
+git commit -m "$msg"
 git push origin dev
