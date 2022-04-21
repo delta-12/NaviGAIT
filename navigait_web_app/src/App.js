88673757b2c57b0736632a2b4a8d014d265d6640
delta-12 <btesq235@gmail.com>
2022-04-21 17:58:39 -0400

    EXPO BABY 2

--- navigait_web_app/src/App.js
+++ navigait_web_app/src/App.js
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard"
 import Upload from "./pages/Upload"
 import Analyze from "./pages/Analyze"
 import Patients from "./pages/Patients"
+import VoteNavigait from "./pages/VoteNavigait"
 
 export default class App extends Component {
 
@@ -16,6 +17,7 @@ export default class App extends Component {
               <Route path="/upload" element={<Upload />} />
               <Route path="/analyze" element={<Analyze />} />
               <Route path="/patients" element={<Patients />} />
+              <Route path="/vote-navigait" element={<VoteNavigait />} />
           </Routes>
       </BrowserRouter>
     )
