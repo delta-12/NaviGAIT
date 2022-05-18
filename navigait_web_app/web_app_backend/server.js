de098b4c5c6a80af8178e38c9c88e6207e5818b4
delta-12 <btesq235@gmail.com>
2022-05-18 11:54:33 -0400

    add cors to web app backend

--- navigait_web_app/web_app_backend/server.js
+++ navigait_web_app/web_app_backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const path = require("path")
+const cors = require("cors")
 const bodyParser = require("body-parser")
 const videos = require("./routes/api/videos")
 const db = process.env.MONGOURI || require("./config/keys").mongoURI
@@ -21,6 +22,7 @@ server.use(
   })
 )
 
+server.use(cors())
 server.use(bodyParser.json())
 
 server.use("/api/videos", videos)
