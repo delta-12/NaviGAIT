895da2f473f0829175e8c73bf32d95be290687a9
delta-12 <btesq235@gmail.com>
2022-05-30 21:07:21 -0400

    button component for upload screen

--- navigait_app/components/Upload/UploadButtonTemplate.js
+++ navigait_app/components/Upload/UploadButtonTemplate.js
@@ -1,10 +1,13 @@
 import { Text, TouchableOpacity } from "react-native"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 
-export const UploadButton = props => {
+export const UploadButtonTemplate = props => {
 
     return (
-        <TouchableOpacity style={{width: props.width, height: props.height, backgroundColor: props.color, justifyContent: "center", alignItems: "center"}}>
+        <TouchableOpacity
+            style={{width: props.width, height: props.height, backgroundColor: props.color, justifyContent: "center", alignItems: "center"}}
+            onPress={props.onPress}
+        >
             <MaterialCommunityIcons name={props.icon} color={"white"} size={20} />
             <Text style={{color: "white"}}>{props.text}</Text>
         </TouchableOpacity>
--- navigait_app/screens/Upload.js
+++ navigait_app/screens/Upload.js
@@ -1,10 +1,9 @@
 import { useState } from "react"
-import { Text, View, TouchableOpacity } from "react-native"
+import { View } from "react-native"
 import { globalStyles } from "../styles/global"
-import { MaterialCommunityIcons } from "@expo/vector-icons"
-import { UploadVideo } from "../components/Upload/UploadVideo"
+// import { UploadVideo } from "../components/Upload/UploadVideo"
 import { UploadTextInput } from "../components/Upload/UploadTextInput"
-import { UploadButton } from "../components/Upload/UploadButton"
+import { UploadButtonTemplate } from "../components/Upload/UploadButtonTemplate"
 
 export function Upload() {
 
@@ -18,19 +17,23 @@ export function Upload() {
   const [patient, setPatient] = useState("")
   const [description, setDescription] = useState("")
 
+  // console.log(data, errors)
+
+  // const upload = () => UploadVideo(setData, setErrors, setLoaded, setShowProgressBar, title, selectedVideo, description, patient)
+
   return (
     <View style={globalStyles.uploadBackground}>
       <View style={globalStyles.uploadCenterBox}>
         <View style={globalStyles.importButtons}>
-          <UploadButton width="45%" height="100%" color="crimson" icon="video" text="Take Video" />
+          <UploadButtonTemplate width="45%" height="100%" color="crimson" icon="video" text="Take Video" />
           <View style={{height: "100%", width: "10%" }}/>
-          <UploadButton width="45%" height="100%" color="crimson" icon="file-import-outline" text="Choose From Library" />
+          <UploadButtonTemplate width="45%" height="100%" color="crimson" icon="file-import-outline" text="Choose From Library" />
         </View>
         <UploadTextInput placeholder="Enter Title" text={title} setText={setTitle} />
         <UploadTextInput placeholder="Enter Patient Name" text={patient} setText={setPatient} />
         <UploadTextInput placeholder="Enter Description" text={description} setText={setDescription} /> 
         <View style={globalStyles.uploadButton}>
-          <UploadButton width="100%" height="100%" color="mediumseagreen" icon="upload" text="Upload" />
+          <UploadButtonTemplate width="100%" height="100%" color="mediumseagreen" icon="upload" text="Upload" onPress={upload} />
         </View>
       </View>
     </View>
