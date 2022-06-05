c7d7dc4ecb57f3282d90decc54f722b8d3dad176
claireolmsted <abcolmsted@gmail.com>
2022-06-05 13:36:30 -0400

    added patient-specific library screen

---
+++ navigait_app/screens/PatientLibrary.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Component } from "react"
+import { View, Text } from "react-native"
+import axios from "axios"
+import { globalStyles } from "../styles/global"
+import { Table } from "../components/Table/Table"
+
+export class PatientLibrary extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Patient Library</Text>
+            </View>
+        )
+    }
+
+}
